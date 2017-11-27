a4651fa848a91ebcf16e40d21199d9be1332c4e1
Adriel Cardoso <adrielcardoso99@gmail.com>
2017-11-27 21:18:33 -0200

    first commit

---
+++ src/main/java/com/doopet/reservationservice/ReservationServiceApplication.java
@@ -0,0 +1,81 @@
+package com.doopet.reservationservice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Arrays;
+import java.util.Collection;
+
+@SpringBootApplication
+public class ReservationServiceApplication {
+
+	@Bean
+	CommandLineRunner runner (ReservationRepository rr){
+		return args -> {
+			Arrays.asList("Dr. Rod, Dr. Syer, All THE COM".split(","))
+					.forEach(x -> rr.save(new Reservation(x)));
+			rr.findAll().forEach(System.out::println);
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+}
+
+
+@RepositoryRestResource
+interface ReservationRepository extends JpaRepository<Reservation, Long>{
+
+	@RestResource(path="by-name")
+	Collection<Reservation> findByReservationName(@Param("rn") String rn);
+}
+
+@Entity
+class Reservation{
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String reservationName;
+
+	public Reservation(){}
+
+	public Reservation(String reservationName) {
+		this.reservationName = reservationName;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation{" +
+				"id=" + id +
+				", reservationName='" + reservationName + '\'' +
+				'}';
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getReservationName() {
+		return reservationName;
+	}
+
+	public void setReservationName(String reservationName) {
+		this.reservationName = reservationName;
+	}
+}
