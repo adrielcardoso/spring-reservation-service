6e8b3b17714f10cc32509758001a052fd2f06eae
Adriel Cardoso <adrielcardoso99@gmail.com>
2017-11-27 22:29:03 -0200

    ajuste params

--- src/main/java/com/doopet/reservationservice/ReservationServiceApplication.java
+++ src/main/java/com/doopet/reservationservice/ReservationServiceApplication.java
@@ -1,14 +1,17 @@
 package com.doopet.reservationservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -33,6 +36,18 @@ public class ReservationServiceApplication {
 	}
 }
 
+@RefreshScope
+@RestController
+class MessageRestController{
+
+	@Value("${message}")
+	private String message;
+
+	@RequestMapping("/message")
+	String message(){
+		return this.message;
+	}
+}
 
 @RepositoryRestResource
 interface ReservationRepository extends JpaRepository<Reservation, Long>{
